072c867ca4bf4b23255c35f83d90083b22bcf4ea
David <dchow2201@gmail.com>
2014-10-09 21:47:19 -0400

    Added interactive plot with linear regression

--- server.R
+++ server.R
@@ -1,16 +1,25 @@
-predictMPG<-function(cylinders,weight){
-  data(mtcars)
+data(mtcars)
+predictMPG<-function(cylinders,weight){  
   mtcars$cyl<-factor(as.character(mtcars$cyl))
-  modelFit<-lm(mpg~cyl+wt,data=mtcars)
+  modelFit<-lm(mpg~cyl*wt,data=mtcars)
   cylinders<-as.character(cylinders)
   mpg=predict(modelFit,data.frame(cyl=cylinders,wt=weight))
   return(mpg[[1]])
 }
+
 shinyServer(
   function(input,output){
     output$inputCyl<-renderPrint({input$Cyl})
     output$inputWt<-renderPrint({input$Wt})
     output$prediction<-renderPrint({predictMPG(input$Cyl,input$Wt)})
-    
+    output$C<-renderPlot({
+      plot(mtcars$wt,mtcars$mpg,xlab="Weight (lb/1000)",
+           ylab="MPG (Miles/(US) gallon)",type="n",
+           main=paste("Linear Regression on MPG vs. Weight for a",
+                      input$Cyl,"cylinders car"))
+      x=(mtcars$cyl==input$Cyl)
+      points(mtcars$wt[x],mtcars$mpg[x],pch=19,col="red")
+      abline(lm(mtcars$mpg[x]~mtcars$wt[x]),col="blue")
+    })
   }  
 )
--- ui.R
+++ ui.R
@@ -1,29 +1,20 @@
 library(shiny)
 pageWithSidebar(
-  headerPanel("The following web site will predict your car's mpg 
-        base on number of cylinders it has and its weight"), 
-  sidebarPanel(    
-    h3('Please enter the number of cylinders your cars has,
-        the choices are 4, 6, or 8'),
+  headerPanel("Shiny Application Project"), 
+  sidebarPanel(
     numericInput('Cyl','Number of cylinders',4,min=4,max=8,step=2),
-    h3('Please specify the weight of your car in lb/1000,
-        example: if your car weight 3000 lb, then you should
-        specify 3'),
     sliderInput('Wt',"Weight (lb/1000)",value=3,min=1.5,max=5.5,step=0.1),    
-    h3('After the number of cylinders and the weight of your car has been
-        specify, click of the submit button below to run the prediction.'),
     submitButton('Submit')    
   ),
   mainPanel(
-    h2("If you have click on the submit button, the prediction of your
-        car's mpg will be shown in the following:"),
-    h3('Prediction result'),
+    h3('Result of prediction'),
     h4('The number of cylinders you entered is'),
     verbatimTextOutput("inputCyl"),
     h4('The Weight (lb/1000) you entered is'),
     verbatimTextOutput("inputWt"),
     h4('The predicted MPG (Miles/(US) gallon) for your car is '),
-    verbatimTextOutput("prediction")
+    verbatimTextOutput("prediction"),
+    plotOutput('C')
   ) 
   
 )
